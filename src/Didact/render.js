a95f47953c167ee7f7a460bd8aa7c699a31a5c41
Yuta123456 <yuuta09090530@icloud.com>
2022-12-18 13:25:10 +0900

    DOM構築とCommitフェーズを分ける

--- src/Didact/render.js
+++ src/Didact/render.js
@@ -15,16 +15,33 @@ export const createDom = (fiber) => {
   return dom
 }
 
+const commitRoot = () => {
+  commitWork(wipRoot.child)
+  wipRoot = null
+}
+const commitWork = (fiber) => {
+  if (!fiber) {
+    return
+  }
+  const domParent = fiber.parent.dom
+  domParent.appendChild(fiber.dom)
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
 export const render = (element, container) => {
-  nextUnitOfWork = {
+  wipRoot = {
     dom: container,
     props: {
       children: [element],
     },
   }
+
+  nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let wipRoot = null
+
 const workLoop = (deadline) => {
   let shouldYield = false
   // console.log("workLoop fire")
@@ -34,18 +51,19 @@ const workLoop = (deadline) => {
     )
     shouldYield = deadline.timeRemaining() < 1
   }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+
   requestIdleCallback(workLoop)
 }
 requestIdleCallback(workLoop)
-
 function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
   }
 
-  if (fiber.parent) {
-    fiber.parent.dom.appendChild(fiber.dom)
-  }
   const elements = fiber.props.children
   let index = 0
   let prevSibling = null
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const element = (
   <div style="background: salmon">
     <h1>Hello World</h1>
     <h2 style="text-align:right">from Didact</h2>
+    <div>
+      <div>
+        <h3>I'm</h3>
+        <h4>Yuta</h4>
+        <h5>Tanaka</h5>
+      </div>
+    </div>
   </div>
 )
 const container = document.getElementById("root")
